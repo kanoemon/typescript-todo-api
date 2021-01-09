5a1e6a1b036f9060d8fd02ea02bd9e07af77bfa7
kanoemon <kanoemon.cho@gmail.com>
2021-01-09 17:27:32 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd  /var/www/typescript-todo-api
 npm install
 npm run build
